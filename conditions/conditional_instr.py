5f4d5154026b1feb356363e8e2193478d2962dba
MikiKru <michal_kruczkowski@o2.pl>
2019-08-24 12:16:56 +0200

    w2.1 v6

--- conditions/conditional_instr.py
+++ conditions/conditional_instr.py
@@ -5,23 +5,56 @@
 #     print("TO NIE JEST LICZBA")
 
 #P49
-login = input("podaj login:")
-password = input("podaj password:")
+# login = input("podaj login:")
+# password = input("podaj password:")
+
+# login, hasło, uprawnienia, aktywacja
+# users = [ ["mk","mk123","ROLE_ADMIN",True],
+#           ["kk", "kk123", "ROLE_USER", True],
+#           ["ll", "ll123", "ROLE_USER", True]]
+
+# LOGOWANIE NA PODSTAWIE LISTY UŻTRKOWNIKÓW
+# isLogged = False
+# for user in users:
+#     if(login == user[0] and password == user[1]):
+#         isLogged = True
+#         if(user[2] == "ROLE_ADMIN"):
+#             print("PANEL ADMINISTRATORA")
+#             break
+#         else:
+#             print("PANEL UŻYTKOWNIKA")
+#             break
+# print("" if isLogged else "BŁĄD LOGOWANIA")
 
 # login, hasło, uprawnienia, aktywacja
 users = [ ["mk","mk123","ROLE_ADMIN",True],
           ["kk", "kk123", "ROLE_USER", True],
           ["ll", "ll123", "ROLE_USER", True]]
 
-# LOGOWANIE NA PODSTAWIE LISTY UŻTRKOWNIKÓW
+login = input("podaj login:")
+
 isLogged = False
 for user in users:
-    if(login == user[0] and password == user[1]):
-        isLogged = True
-        if(user[2] == "ROLE_ADMIN"):
-            print("PANEL ADMINISTRATORA")
-            break
+    # spr. czy jest taki użytkownik
+    if(login == user[0] and user[3] == True):
+        password = input("podaj password:")
+        # spr. czy ma poprawne hasło
+        if(password == user[1]):
+            isLogged = True
+            # sprawdzam uprawnienia
+            if (user[2] == "ROLE_ADMIN"):
+                print("PANEL ADMINISTRATORA")
+                break
+            else:
+                print("PANEL UŻYTKOWNIKA")
+                break
         else:
-            print("PANEL UŻYTKOWNIKA")
-            break
+            print("BŁĘDNE HASŁO")
+    elif(login == user[0] and user[3] == False):
+        print("KONTO ZABLOKOWANE!")
+
 print("" if isLogged else "BŁĄD LOGOWANIA")
+
+
+
+
