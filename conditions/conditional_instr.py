7c2292ba030cecc40cf0100e1e32ff6a0d420c88
MikiKru <michal_kruczkowski@o2.pl>
2019-08-24 12:21:00 +0200

    w2.1 v7

--- conditions/conditional_instr.py
+++ conditions/conditional_instr.py
@@ -31,29 +31,30 @@ users = [ ["mk","mk123","ROLE_ADMIN",True],
           ["kk", "kk123", "ROLE_USER", True],
           ["ll", "ll123", "ROLE_USER", True]]
 
-login = input("podaj login:")
 
 isLogged = False
-for user in users:
-    # spr. czy jest taki użytkownik
-    if(login == user[0] and user[3] == True):
-        password = input("podaj password:")
-        # spr. czy ma poprawne hasło
-        if(password == user[1]):
-            isLogged = True
-            # sprawdzam uprawnienia
-            if (user[2] == "ROLE_ADMIN"):
-                print("PANEL ADMINISTRATORA")
-                break
+badCredentialsCounter = 0
+while(isLogged == False):
+    login = input("podaj login:")
+    for user in users:
+        # spr. czy jest taki użytkownik
+        if(login == user[0] and user[3] == True):
+            password = input("podaj password:")
+            # spr. czy ma poprawne hasło
+            if(password == user[1]):
+                isLogged = True
+                # sprawdzam uprawnienia
+                if (user[2] == "ROLE_ADMIN"):
+                    print("PANEL ADMINISTRATORA")
+                    break
+                else:
+                    print("PANEL UŻYTKOWNIKA")
+                    break
             else:
-                print("PANEL UŻYTKOWNIKA")
-                break
-        else:
-            print("BŁĘDNE HASŁO")
-    elif(login == user[0] and user[3] == False):
-        print("KONTO ZABLOKOWANE!")
+                print("BŁĘDNE HASŁO")
+        elif(login == user[0] and user[3] == False):
+            print("KONTO ZABLOKOWANE!")
 
-print("" if isLogged else "BŁĄD LOGOWANIA")
 
 
 
