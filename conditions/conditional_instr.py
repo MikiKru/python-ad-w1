39367b4dbf3c3f44d9e99a623456b9b36b2eb5bd
MikiKru <michal_kruczkowski@o2.pl>
2019-08-24 12:34:25 +0200

    w2.1 v9

--- conditions/conditional_instr.py
+++ conditions/conditional_instr.py
@@ -27,13 +27,12 @@
 # print("" if isLogged else "BŁĄD LOGOWANIA")
 
 # login, hasło, uprawnienia, aktywacja
-users = [ ["mk","mk123","ROLE_ADMIN",True],
-          ["kk", "kk123", "ROLE_USER", True],
-          ["ll", "ll123", "ROLE_USER", True]]
+users = [ ["mk","mk123","ROLE_ADMIN",True,0],
+          ["kk", "kk123", "ROLE_USER", True,0],
+          ["ll", "ll123", "ROLE_USER", True,0]]
 
 
 isLogged = False
-badCredentialsCounter = 0
 while(isLogged == False):
     print(users)
     login = input("podaj login:")
@@ -53,12 +52,11 @@ while(isLogged == False):
                     break
             else:
                 print("BŁĘDNE HASŁO")
-                badCredentialsCounter += 1
-                if (badCredentialsCounter == 3):
+                user[4] += 1
+                if (user[4] == 3):
                     user[3] = False
         elif(login == user[0] and user[3] == False):
             print("KONTO ZABLOKOWANE!")
-            isLogged = True
             break
 
 
