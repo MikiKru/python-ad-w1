b7f2bd50bdccad396dcad6a2b1970daa2671235a
MikiKru <michal_kruczkowski@o2.pl>
2019-08-25 09:10:54 +0200

    w2.2 v3

--- loops/loops_script.py
+++ loops/loops_script.py
@@ -100,26 +100,29 @@ while(isClosed == False):
     print("%3s | %20s | %5s | %3s" % ("ID","NAZWA","CENA","ILOŚC"))
     for key in products.keys():                         # pętla wypisująca zawartość słownika products
         print("%3i | %20s | %5.2f | %3i" % (key,products[key][0],products[key][1],products[key][2]))
-    decision = int(input("Wybierz produkt 0-wyjście"))  # wybór użytkownika -> klucz produktu
-    if(decision == 0):                                  # wyjście z programu
-        sum = 0
-        for key in order.keys():                        # pętla obliczająca sumę do zapłaty na podstawie
-            sum += order[key][1] * order[key][2]        # słownika order
-        print("Do zapłaty: " + str(sum) + "zł")
-        isClosed = True
-    elif(decision in products.keys()):                  # wybór produktu
-        amount = int(input("Podaj ilość"))              # wybór użytkownika -> ilość zamawiana produktu
-        if(products[decision][2] < amount):             # porównanie ilości z magazynem -> za dużo jest zamawiane
-            print("Brak takiej ilości w magazynie")
-        else:                                           # porównanie ilości z magazynem -> zamówienie przyjęte
-            print("Dodano do koszyka")
-            order[decision] = [products[decision][0],products[decision][1],amount] # dodanie zamówienia do koszyka
-            print("Twój koszyk")
-            for key in order.keys():                    # pętla wypisująca zawartość koszyka
-                print("%3i | %20s | %5.2f | %3i" % (key, order[key][0], order[key][1], order[key][2]))
-            products[decision][2] = products[decision][2] - amount
-    else:                                               # błąd użytkownika
-        print("Błędny wybór")
+    try:
+        decision = int(input("Wybierz produkt 0-wyjście"))  # wybór użytkownika -> klucz produktu
+        if(decision == 0):                                  # wyjście z programu
+            sum = 0
+            for key in order.keys():                        # pętla obliczająca sumę do zapłaty na podstawie
+                sum += order[key][1] * order[key][2]        # słownika order
+            print("Do zapłaty: " + str(sum) + "zł")
+            isClosed = True
+        elif(decision in products.keys()):                  # wybór produktu
+            amount = int(input("Podaj ilość"))              # wybór użytkownika -> ilość zamawiana produktu
+            if(products[decision][2] < amount):             # porównanie ilości z magazynem -> za dużo jest zamawiane
+                print("Brak takiej ilości w magazynie")
+            else:                                           # porównanie ilości z magazynem -> zamówienie przyjęte
+                print("Dodano do koszyka")
+                order[decision] = [products[decision][0],products[decision][1],amount] # dodanie zamówienia do koszyka
+                print("Twój koszyk")
+                for key in order.keys():                    # pętla wypisująca zawartość koszyka
+                    print("%3i | %20s | %5.2f | %3i" % (key, order[key][0], order[key][1], order[key][2]))
+                products[decision][2] = products[decision][2] - amount
+        else:                                               # błąd użytkownika
+            print("Błędny wybór")
+    except:
+        print("Błąd! Musisz podać wartość całkowitą")
 
 
 
