1acfe9940e552c5ab1d4b3fb2592dcf02822cf64
MikiKru <michal_kruczkowski@o2.pl>
2019-08-25 12:45:19 +0200

    w2.2 v11

--- methods/methods.py
+++ methods/methods.py
@@ -1,4 +1,5 @@
 import datetime
+from math import sqrt
 from random import choices
 from time import localtime, time, ctime
 
@@ -149,8 +150,11 @@ def generateSentence(content, n = 5):
 
 print(generateSentence(content))
 
+# P60
+def distanceMeasure(x_start, y_start, x_stop, y_stop):
+    return sqrt(pow(x_stop - x_start, 2) + pow(y_stop - y_start, 2))
 
-
+print(distanceMeasure(1,1,2,2))
 
 
 
