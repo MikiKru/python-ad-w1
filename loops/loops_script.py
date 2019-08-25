f5e3cb030bd7badb16ed697ff6bc8d62568e343c
MikiKru <michal_kruczkowski@o2.pl>
2019-08-25 09:20:09 +0200

    w2.2 v4

--- loops/loops_script.py
+++ loops/loops_script.py
@@ -95,11 +95,13 @@ order = {}
 4. Eksport paragonu wraz z kwotą do zapłaty
 """
 isClosed = False                                        # zmienna decydująca o zakończeniu programu
+orderNo = 1
 while(isClosed == False):
     print("Wybierz produkt podając jego ID")
     print("%3s | %20s | %5s | %3s" % ("ID","NAZWA","CENA","ILOŚC"))
-    for key in products.keys():                         # pętla wypisująca zawartość słownika products
-        print("%3i | %20s | %5.2f | %3i" % (key,products[key][0],products[key][1],products[key][2]))
+    for key in products.keys():                             # pętla wypisująca zawartość słownika products gdy są na magazynie
+        if(products[key][2] != 0):
+            print("%3i | %20s | %5.2f | %3i" % (key,products[key][0],products[key][1],products[key][2]))
     try:
         decision = int(input("Wybierz produkt 0-wyjście"))  # wybór użytkownika -> klucz produktu
         if(decision == 0):                                  # wyjście z programu
@@ -114,7 +116,8 @@ while(isClosed == False):
                 print("Brak takiej ilości w magazynie")
             else:                                           # porównanie ilości z magazynem -> zamówienie przyjęte
                 print("Dodano do koszyka")
-                order[decision] = [products[decision][0],products[decision][1],amount] # dodanie zamówienia do koszyka
+                order[orderNo] = [products[decision][0],products[decision][1],amount] # dodanie zamówienia do koszyka
+                orderNo += 1
                 print("Twój koszyk")
                 for key in order.keys():                    # pętla wypisująca zawartość koszyka
                     print("%3i | %20s | %5.2f | %3i" % (key, order[key][0], order[key][1], order[key][2]))
