fdc9326d22cc52687cd82d9a0b049cb6a4281e56
MikiKru <michal_kruczkowski@o2.pl>
2019-09-07 14:34:49 +0200

    d4 -v14

--- oop/heridity.py
+++ oop/heridity.py
@@ -7,20 +7,30 @@ class User:
         if(self.permissionSelect):
             print("READING...")
     def __str__(self):
-        return "login: %s password: %s PERMISSIONS: %s" \
-               %(self.login,self.password, ("SELECT" if (self.permissionSelect == True) else ""))
+        return "login: %s password: %s PERMISSIONS: %s " \
+               %(self.login,self.password, ("SELECT" if self.permissionSelect else ""))
 
 # moderator dziedziczy po obiekcie user
 class Moderator(User):
     def __init__(self, login, password):
-        self.login = login
-        self.password = password
+        # wywołanie metody __init__() klasy nadrzędnej
+        super().__init__(login, password)
         self.permissionInsert = True
+    def writePost(self):
+        if(self.permissionInsert):
+            print("WRITING...")
+    def __str__(self):
+        # wysołanie metody __str__() klasy nadrzędnej
+        return super().__str__() + "INSERT" if self.permissionInsert else ""
 
 user = User("user", "user")
 print(user)
+user.readPost()
 
-
+moderator = Moderator("moderator","moderator")
+print(moderator)
+moderator.readPost()
+moderator.writePost()
 
 
 # admin dziedziczy po obiekcie moderator
