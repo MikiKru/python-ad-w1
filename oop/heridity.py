b0aeed2d456c47ae17207021cf06bb5fc049cad3
MikiKru <michal_kruczkowski@o2.pl>
2019-09-07 14:44:12 +0200

    d4 -v15

--- oop/heridity.py
+++ oop/heridity.py
@@ -22,18 +22,35 @@ class Moderator(User):
     def __str__(self):
         # wysołanie metody __str__() klasy nadrzędnej
         return super().__str__() + "INSERT" if self.permissionInsert else ""
+# admin dziedziczy po obiekcie moderator
+class Admin(Moderator):
+    def __init__(self, login, password):
+        super().__init__(login, password)
+        self.permissionDelete = True
+        self.permissionUpdate = True
+    def updatePost(self):
+        print("UPDATING...")
+    def deletePost(self):
+        print("DELETING...")
+    def __str__(self):
+        return super().__str__() + \
+               (" DELETE" if self.permissionDelete else "")+(" UPDATE" if self.permissionUpdate else "")
 
+
+print("### USER ###")
 user = User("user", "user")
 print(user)
 user.readPost()
-
+print("### MODERATOR ###")
 moderator = Moderator("moderator","moderator")
 print(moderator)
 moderator.readPost()
 moderator.writePost()
-
-
-# admin dziedziczy po obiekcie moderator
-class Admin(Moderator):
-    pass
+print("### ADMIN ###")
+admin = Admin("admin", "admin")
+print(admin)
+admin.readPost()
+admin.writePost()
+admin.updatePost()
+admin.deletePost()
 
