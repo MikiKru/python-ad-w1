54311c10d069aeabb1d0e5e5e83c0e83eed06934
MikiKru <michal_kruczkowski@o2.pl>
2019-09-08 10:10:04 +0200

    d4 -v25

--- homework/main.py
+++ homework/main.py
@@ -9,11 +9,16 @@ while(True):
                  "(D)-dodaj nowego studenta\n(U)-usuń studenta\n(Z)-zaktualizuj oceny\n"
                  "(O)-wyczyść listę ocen studenta\n(W)-wypisz listę studentów\n(Q)-wyjdź z programu")
     if(menu.upper()=="D"):
-        name = input("podaj imię")
-        lastname = input("podaj nazwisko")
+        name = input("podaj imię: ")
+        lastname = input("podaj nazwisko: ")
         dziekanat.addStudent(name,lastname)
     elif(menu.upper()=="U"):
-        pass
+        print(dziekanat)
+        try:
+            index_no = int(input("podaj numer indeksu"))
+            dziekanat.deleteStudentByIndex(index_no)
+        except:
+            print("Numer indeksu musi być liczbą!")
     elif(menu.upper()=="Z"):
         pass
     elif(menu.upper()=="O"):
