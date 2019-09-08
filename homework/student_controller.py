c7afd4beb227be2fed5732a2795d0823f24764a0
MikiKru <michal_kruczkowski@o2.pl>
2019-09-08 09:31:29 +0200

    d4 -v22

--- homework/student_controller.py
+++ homework/student_controller.py
@@ -13,9 +13,35 @@ class StudentController:
     def addStudent(self, name, lastname):
         student = Student(name, lastname)
         self.students.append(student)
+    # metoda szukająca i zwracająca studenta
+    def findStudentByIndex(self,index_no):
+        for student in self.students:
+            if(index_no == student.index_no):
+                return student
+        return None
+    # metoda usuwająca studenta z listy
+    def deleteStudentByIndex(self,index_no):
+        deletedStudent = self.findStudentByIndex(index_no)
+        if(deletedStudent != None):
+            self.students.remove(deletedStudent)
+            print("usunięto studenta " + deletedStudent.__str__())
+        else:
+            print("Nie ma takiego studenta")
+    # dodawanie ocen do studenta
+    def addGradesToStudent(self, index_no, new_grades):
+        findStudent = self.findStudentByIndex(index_no)
+        if(findStudent != None):
+            findStudent.grades.extend(new_grades)
+            print("zaktualizowano listę ocen studenta")
+    def deleteStudentGrades(self, index_no):
+        findStudent = self.findStudentByIndex(index_no)
+        if (findStudent != None):
+            findStudent.grades = []
+            print("wyczyszczona lista ocen studenta")
 
-dziekanat = StudentController()
-dziekanat.addStudent("test", "test")
-dziekanat.addStudent("test", "test")
-dziekanat.addStudent("test", "test")
-print(dziekanat)
+
+
+
+
+
+
